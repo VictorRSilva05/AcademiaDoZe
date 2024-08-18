34cbf2b74f3fcfdb95d5a7b27824e405e485c012
Victor R. Silva <victorr.silva005@gmail.com>
2024-08-18 12:52:16 -0300

    Translated Matricula screen

--- AcademiaDoZe_WPF/CadastrarMatriculaControl.xaml.cs
+++ AcademiaDoZe_WPF/CadastrarMatriculaControl.xaml.cs
@@ -11,6 +11,12 @@ namespace AcademiaDoZe_WPF
         public CadastrarMatriculaControl()
         {
             InitializeComponent();
+            this.Loaded += CadastrarMatriculaControl_Loaded;
+        }
+
+        private void CadastrarMatriculaControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ClassFuncoes.AjustaResources(this);
         }
 
         private void checkbox_problemas_cardiacos_Checked(object sender, RoutedEventArgs e)
